29d2ce11236c718c8f45ae243a4bc1b51cf2862c
Jeff <okayell@hotmail.com>
2020-12-23 22:08:11 +0800

    First commit. Finish Ch4

---
+++ mynewsite/mysite/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from mysite.models import NewTable
+from mysite.models import Product
+
+# Register your models here.
+admin.site.register(NewTable)
+admin.site.register(Product)
---
+++ mynewsite/mysite/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class NewTable(models.Model):
+    bigint_f = models.BigIntegerField()
+    bool_f = models.BooleanField()
+    date_f = models.DateField(auto_now=True)
+    char_f = models.CharField(max_length=20)
+
+
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
+    qty = models.PositiveIntegerField(default=10)
+
+    def __str__(self):
+        return self.name
